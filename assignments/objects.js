7e302e8d5b52e681724f7b90ad016522e35dffa2
vchocou <56698114+vchocou@users.noreply.github.com>
2019-10-17 12:37:58 -0700

    mitzi object reading test

--- assignments/objects.js
+++ assignments/objects.js
@@ -18,12 +18,19 @@ const example = {
 }
 
 // Write your intern objects here:
+const mitzi = {
+  id: 1,
+  name: "Mitzi",
+  email: "mmelloy0@psu.edu",
+  gender: "F"
+}
 
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+console.log(mitzi.name);
 
 // Kennan's ID
 
